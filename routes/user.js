519d2454b995ca9957a0af5cb496ee88dbd23d04
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-11-04 18:58:01 +0900

    userChange

--- routes/user.js
+++ routes/user.js
@@ -63,17 +63,6 @@ function createJwt(name, key) {
 //   })
 //   .catch((err) => console.error(err));
 
-db.userdb.sequelize
-  .sync({ force: false })
-  // db 서버와 연결한다, force는 설정된 테이블을 강제로 생성한다.
-  // 우리가 express 서버에서 설정한 테이블 데이터와 실제 DB서버의 테이블 데이터가 다를 경우에 서버의 테이블을 새로 생성하기 위해 사용한다.
-  .then((data) => {
-    console.log(`db connected`);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 // userdb create 양식
 
 // db.userdb.UserTable.create({
