1adb6eab8557d8cee76e1888a74c1a06f293f6fc
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-11-07 21:33:11 +0900

    modal test

--- routes/notice.js
+++ routes/notice.js
@@ -16,6 +16,21 @@ router
   });
 
 router
+  .route("/askRequest")
+  .get((req, res) => {
+    res.send();
+  })
+  .post((req, res) => {
+    db.AskanswerTable.create({
+      userId: req.body.userId,
+      name: req.body.name,
+      text: req.body.text,
+      isAnswer: 0,
+    });
+    res.send("create fin");
+  });
+
+router
   .route("/askanswer")
   .get((req, res) => {
     res.send();
@@ -37,6 +52,14 @@ router
     });
     res.send(tempData);
   });
+
+router.route("/productask").post((req, res) => {
+  db.ProductaskTable.findAll({
+    where: { product_name: req.body.productName },
+  }).then((data) => {
+    res.send(data);
+  });
+});
 
 function getToday() {
   var date = new Date();
