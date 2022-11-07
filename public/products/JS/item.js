159529be3732ef8ad677e0974f5e9bde91c2c9b2
kimsj <sophia10047600@gmail.com>
2022-11-07 10:14:58 +0900

    switch

--- public/products/JS/item.js
+++ public/products/JS/item.js
@@ -5,13 +5,8 @@ getList();
 
 async function getList() {
   try {
-    console.log("hi");
     const tempDiv = document.createElement("div");
-    tempDiv.style = `
-    width : 100px;
-    height : 100px;
-    background-color: blue;
-  `;
+
     const childDiv = document.createElement("div");
     childDiv.style = `
     width : 50px;
@@ -22,6 +17,7 @@ async function getList() {
     itemList.appendChild(tempDiv);
     // const result = (await axios.get("/api/vegi")).data;
     // itemList.innerHTML = "";
+
     const tempUl = document.createElement("ul");
     tempUl.style = `
     display: flex;
@@ -32,9 +28,11 @@ async function getList() {
     `;
     const tempLi = document.createElement("li");
     tempLi.style = `
-    width: 800px;
+    width: 290px;
     height: 450px;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
     `;
     tempLi.innerHTML = `<img src="/api/product/download20" alt="" />
     <p>가격</p>
@@ -53,8 +51,8 @@ async function getList() {
     <span><img src="/imges/cart3.svg" alt="" /></span>`;
 
     itemList.getElementsByTagName("ul")[0].append(tempLi);
-    const productItem = document.createElement("div");
-    const innerItem = document.createElement("div");
+    // const productItem = document.createElement("div");
+    // const innerItem = document.createElement("div");
   } catch (error) {
     console.log(error);
   }
@@ -62,29 +60,28 @@ async function getList() {
 
 axios.post("/api/product/getCategory", { category: "야채" }).then((data) => {
   console.log(data);
-  const tempDiv = document.createElement("div");
-  tempDiv.style = `
-    width : 100px;
-    height : 100px;
-    background-color: blue;
-  `;
-  const childDiv = document.createElement("div");
-  childDiv.style = `
-    width : 50px;
-    height : 30px;
-    background-color: pink;
-  `;
-  tempDiv.appendChild(childDiv);
-  itemList.appendChild(tempDiv);
-
-  const tempLi = document.createElement("li");
-  tempLi.innerHTML = `<img src="/api/product/download20" alt="" />
-    <p>이름 : ${data.data[0].name}</p>
-    <p>가격</p>
-    <p>정보</p>
-    <span><img src="/imges/cart3.svg" alt="" /></span>`;
-
-  itemList.getElementsByTagName("ul")[0].append(tempLi);
+  // const tempDiv = document.createElement("div");
+  // tempDiv.style = `
+  //     width : 100px;
+  //     height : 100px;
+  //     background-color: blue;
+  //   `;
+  // const childDiv = document.createElement("div");
+  // childDiv.style = `
+  //     width : 50px;
+  //     height : 30px;
+  //     background-color: pink;
+  //     display : flex;
+  //   `;
+  //   tempDiv.appendChild(childDiv);
+  //   itemList.appendChild(tempDiv);
+  //   const tempLi = document.createElement("li");
+  //   tempLi.innerHTML = `<img src="/api/product/download20" alt="" />
+  //     <p>이름 : ${data.data[0].name}</p>
+  //     <p>가격</p>
+  //     <p>정보</p>
+  //     <span><img src="/imges/cart3.svg" alt="" /></span>`;
+  //   itemList.getElementsByTagName("ul")[0].append(tempLi);
 });
 
 // function 요청(categoryname){return 원하는 리스트}
