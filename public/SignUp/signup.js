63c16bc2bbcd56df25f0e7224077b84eaefcea97
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-11-08 12:44:43 +0900

    git remove 주소검색 다시 제외

--- public/SignUp/signup.js
+++ public/SignUp/signup.js
@@ -69,13 +69,3 @@ document.getElementById("signup_btn_btn").onclick = async () => {
     console.error(error);
   }
 };
-// 주소검색 api
-document.getElementById("signup_board_address_btn").onclick = () => {
-  new daum.Postcode({
-    oncomplete: function (data) {
-      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-      // 예제를 참고하여 다양한 활용법을 확인해 보세요.
-      console.log(data.address);
-    },
-  }).open();
-};
