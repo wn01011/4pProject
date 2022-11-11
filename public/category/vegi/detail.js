9cec2f886301167d121a70a1b243b131b93dd6c0
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-11-11 10:24:53 +0900

    vegi filter

--- public/category/vegi/detail.js
+++ public/category/vegi/detail.js
@@ -129,24 +129,78 @@ const getList = function (
 const brandFilter = document.getElementById("filter-brand");
 const priceBrand = document.getElementById("brand-price");
 let brandSet = new Set();
+const checkList = document.getElementsByClassName("check-list");
+checkList[0].onclick = () => {
+  if (selectedPrice == 0) selectedPrice = -1;
+  else selectedPrice = 0;
+  pricesFilter();
+  searchFunc();
+};
+checkList[1].onclick = () => {
+  if (selectedPrice == 1) selectedPrice = -1;
+  else selectedPrice = 1;
+  pricesFilter();
+  searchFunc();
+};
+checkList[2].onclick = () => {
+  if (selectedPrice == 2) selectedPrice = -1;
+  else selectedPrice = 2;
+  pricesFilter();
+  searchFunc();
+};
+checkList[3].onclick = () => {
+  if (selectedPrice == 3) selectedPrice = -1;
+  else selectedPrice = 3;
+  pricesFilter();
+  searchFunc();
+};
 // let filterSet = new Set();
 
 const getFilter = function (manufacturer) {
   try {
     const filterLi = document.createElement("li");
     const filterImg = document.createElement("img");
-    const filterA = document.createElement("a");
-    const filterAa = document.createElement("a");
+    const filterA = document.createElement("button");
+    const filterAa = document.createElement("button");
 
     filterImg.src = `/category/imges/detailImg/check-circle.svg`;
+    filterA.style = `
+border : none;
+background-color : transparent;
+border-radius: 50%;
+padding : 0;
+`;
+
     filterAa.innerText = `${manufacturer}`;
     filterAa.style = `
-    margin-left: 20px;
-    line-height: 2
-    `;
+margin-left: 20px;
+line-height: 2;
+border : none;
+background-color : transparent; 
+`;
     filterImg.style = `
-    opacity: 0.3;
-    `;
+opacity: 0.7;
+`;
+    filterLi.onclick = () => {
+      if (selectedBrand.includes(filterAa.innerText)) {
+        const tempAry = [];
+        selectedBrand.filter((elem) => {
+          if ((elem, filterAa.innerText, elem !== filterAa.innerText)) {
+            tempAry.push(elem);
+          }
+          elem !== filterAa.innerText;
+        });
+        selectedBrand = tempAry;
+      } else {
+        selectedBrand.push(filterAa.innerText);
+      }
+      if (selectedBrand.includes(filterAa.innerText)) {
+        filterA.style.backgroundColor = "rgba(75, 0, 130, 0.7)";
+      } else {
+        filterA.style.backgroundColor = "transparent";
+      }
+      searchFunc();
+    };
 
     brandFilter.append(filterLi);
     filterA.append(filterImg);
@@ -159,6 +213,15 @@ const getFilter = function (manufacturer) {
 
 const vegiCategories = function (category) {
   try {
+    categoryAll.innerHTML = "";
+    categoryBro.innerHTML = "";
+    categoryBean.innerHTML = "";
+    categoryEnv.innerHTML = "";
+    categoryOni.innerHTML = "";
+    categoryPota.innerHTML = "";
+    categoryCuc.innerHTML = "";
+    categorySpi.innerHTML = "";
+    categoryFro.innerHTML = "";
     const tempAllA = document.createElement("a");
     const tempBroA = document.createElement("a");
     const tempBeanA = document.createElement("a");
@@ -201,46 +264,87 @@ const filter = document.getElementById("filter");
 const list = document.getElementById("list");
 const totalCount = document.getElementById("totalCount");
 const productNone = document.getElementById("productNone");
-axios
-  .post("/api/product/category", {
-    data: "채소",
-    brand: selectedBrand,
-    price: selectedPrice,
-  })
-  .then((data) => {
-    totalCount.innerText = "총 " + data.data.length + "건";
-    vegiCategories();
-    console.log(data.data);
-    data.data.forEach((item) => {
-      // 카테고리별 아이템 추출
-      const category = Object.values(item.category[0]);
-      console.log(category[1]);
-      getList(
-        item.img,
-        item.delivery,
-        item.name,
-        item.description,
-        item.price,
-        item.manufacturer,
-        category
-      );
-      // 브랜드 추출
-      if (!brandSet.has(item.manufacturer) && item.manufacturer != "") {
-        brandSet.add(item.manufacturer);
-        getFilter(item.manufacturer);
+function searchFunc() {
+  goods.innerHTML = "";
+  axios
+    .post("/api/product/category", {
+      data: "채소",
+      brand: selectedBrand,
+      price: selectedPrice,
+    })
+    .then((data) => {
+      totalCount.innerText = "총 " + data.data.length + "건";
+      vegiCategories();
+      data.data.forEach((item) => {
+        // 카테고리별 아이템 추출
+        const category = Object.values(item.category[0]);
+        getList(
+          item.img,
+          item.delivery,
+          item.name,
+          item.description,
+          item.price,
+          item.manufacturer,
+          category
+        );
+        // 브랜드 추출
+        if (!brandSet.has(item.manufacturer) && item.manufacturer != "") {
+          brandSet.add(item.manufacturer);
+          getFilter(item.manufacturer);
+        }
+      });
+      if (data.data.length == 0) {
+        productNone.style.display = "flex";
+        list.style.display = "none";
+        if (selectedBrand.length == 0 && selectedPrice == -1)
+          filter.style.display = "none";
+      } else {
+        productNone.style.display = "none";
+        list.style.display = "block";
+        filter.style.display = "block";
       }
+    })
+    .catch((err) => {
+      console.error(err);
     });
-    if (data.data.length == 0) {
-      productNone.style.display = "flex";
-      list.style.display = "none";
-      if (selectedBrand.length == 0 && selectedPrice == -1)
-        filter.style.display = "none";
-    } else {
-      productNone.style.display = "none";
-      list.style.display = "block";
-      filter.style.display = "block";
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+}
+searchFunc();
+
+const pricesFilter = function () {
+  switch (selectedPrice) {
+    case -1:
+      [...checkList].forEach((item) => {
+        const currImg = item.getElementsByTagName("img")[0];
+        currImg.style.backgroundColor = "transparent";
+      });
+      break;
+    case 0:
+      [...checkList].forEach((item, idx) => {
+        const currImg = item.getElementsByTagName("img")[0];
+        currImg.style.backgroundColor = "transparent";
+        if (idx == 0) currImg.style.backgroundColor = "rgba(75, 0, 130, 0.7)";
+      });
+      break;
+    case 1:
+      [...checkList].forEach((item, idx) => {
+        const currImg = item.getElementsByTagName("img")[0];
+        currImg.style.backgroundColor = "transparent";
+        if (idx == 1) currImg.style.backgroundColor = "rgba(75, 0, 130, 0.7)";
+      });
+      break;
+    case 2:
+      [...checkList].forEach((item, idx) => {
+        const currImg = item.getElementsByTagName("img")[0];
+        currImg.style.backgroundColor = "transparent";
+        if (idx == 2) currImg.style.backgroundColor = "rgba(75, 0, 130, 0.7)";
+      });
+      break;
+    case 3:
+      [...checkList].forEach((item, idx) => {
+        const currImg = item.getElementsByTagName("img")[0];
+        currImg.style.backgroundColor = "transparent";
+        if (idx == 3) currImg.style.backgroundColor = "rgba(75, 0, 130, 0.7)";
+      });
+      break;
+  }
+};
